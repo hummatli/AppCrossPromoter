571a39cc597c6e64793aba8a82b07e4ec1d5f725
Sattar Hummatli <settarxan@gmail.com>
2016-07-03 17:34:36 +0400

    test

--- MAHAds/mah-ads/src/main/java/com/mobapphome/mahads/tools/DBRequesterListener.java
+++
@@ -1,9 +0,0 @@
-package com.mobapphome.mahads.tools;
-
-import java.util.List;
-
-import com.mobapphome.mahads.types.Program;
-
-public interface DBRequesterListener {
-	public void onReadPrograms(List<Program> programs);
-}
