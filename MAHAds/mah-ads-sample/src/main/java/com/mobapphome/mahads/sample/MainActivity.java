9540962f3246ef56a2f6da1b9b23ee8f0346b481
Sattar Hummatli <settarxan@gmail.com>
2016-09-02 14:21:57 +0400

    Added lacalization support

--- MAHAds/mah-ads-sample/src/main/java/com/mobapphome/mahads/sample/MainActivity.java
+++ MAHAds/mah-ads-sample/src/main/java/com/mobapphome/mahads/sample/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.mobapphome.mahads.tools.LocaleUpdater;
 import com.mobapphome.mahads.tools.MAHAdsController;
 import com.mobapphome.mahads.tools.MAHAdsExitListener;
 
@@ -12,6 +14,7 @@ public class MainActivity extends AppCompatActivity implements MAHAdsExitListene
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LocaleUpdater.setLocale(this,"tr");
         setContentView(R.layout.activity_main);
         // For MAHAds init
         MAHAdsController.init(this,"http://ubuntu1mah.cloudapp.net/mah_ads_dir/");
---
+++ MAHAds/mah-ads/src/main/java/com/mobapphome/mahads/tools/LocaleUpdater.java
@@ -0,0 +1,24 @@
+package com.mobapphome.mahads.tools;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.preference.PreferenceManager;
+
+import java.util.Locale;
+
+public class LocaleUpdater {
+
+    public static void setLocale(Context context, String language) {
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+
+        Resources resources = context.getResources();
+
+        Configuration configuration = resources.getConfiguration();
+        configuration.locale = locale;
+
+        resources.updateConfiguration(configuration, resources.getDisplayMetrics());
+    }
+}
